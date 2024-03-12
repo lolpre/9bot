8e2305d0f153a63ca03f0e465a1c70d1af5184fb
Ethan Joseph <ethanj217@gmail.com>
2024-03-11 23:36:21 -0400

    extract handlers

---
+++ src/handlers/events.ts
@@ -0,0 +1,21 @@
+import { Client } from "discord.js";
+import fs from "fs";
+import path from "path";
+
+module.exports = (client: Client) => {
+  const eventsPath = path.join(__dirname, "../events");
+  const eventFiles = fs
+    .readdirSync(eventsPath)
+    .filter((file) => file.endsWith(".js"));
+
+  for (const file of eventFiles) {
+    const filePath = path.join(eventsPath, file);
+    const event = require(filePath).default;
+    console.log(`Loading ${file}`);
+    if (event.once) {
+      client.once(event.name, (...args: any[]) => event.execute(...args));
+    } else {
+      client.on(event.name, (...args: any[]) => event.execute(...args));
+    }
+  }
+};
--- src/index.ts
+++ src/index.ts
@@ -6,44 +6,11 @@ import path from "path";
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 client.commands = new Collection();
 
-const foldersPath = path.join(__dirname, "commands");
-const commandFolders = fs.readdirSync(foldersPath);
-
-for (const folder of commandFolders) {
-  const commandsPath = path.join(foldersPath, folder);
-  const commandFiles = fs
-    .readdirSync(commandsPath)
-    .filter((file) => file.endsWith(".js"));
-  for (const file of commandFiles) {
-    const filePath = path.join(commandsPath, file);
-    const command = require(filePath).default;
-    console.log(`Loading ${file}`);
-    // Set a new item in the Collection with the key as the command name and the value as the exported module
-    if ("data" in command && "execute" in command) {
-      client.commands.set(command.data.name, command);
-    } else {
-      console.log(
-        `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`,
-      );
-    }
-  }
-}
-
-const eventsPath = path.join(__dirname, "events");
-const eventFiles = fs
-  .readdirSync(eventsPath)
-  .filter((file) => file.endsWith(".js"));
-
-for (const file of eventFiles) {
-  const filePath = path.join(eventsPath, file);
-  const event = require(filePath).default;
-  console.log(`Loading ${file}`);
-  if (event.once) {
-    client.once(event.name, (...args: any[]) => event.execute(...args));
-  } else {
-    client.on(event.name, (...args: any[]) => event.execute(...args));
-  }
-}
+const handlersDir = path.join(__dirname, "./handlers");
+fs.readdirSync(handlersDir).forEach((handler) => {
+  if (!handler.endsWith(".js") && !handler.endsWith(".ts")) return;
+  require(`${handlersDir}/${handler}`)(client);
+});
 
 // Log in to Discord with your client's token
 client.login(config.token);
